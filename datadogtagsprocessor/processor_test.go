ee68fb736591f127815253c1c3c7c168b52ebfdd
Vasileios Pallas <vasileios.pallas@flyr.com>
2026-08-02 22:03:04 +0200

    test: integratio tests

--- datadogtagsprocessor/processor_test.go
+++ datadogtagsprocessor/processor_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/FLYR-Open-Source/datadogtagsprocessor/datadogtagsprocessor/internal/metadata"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/plogtest"
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/ptracetest"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/consumer/consumertest"
 	"go.opentelemetry.io/collector/processor/processortest"
@@ -101,4 +102,94 @@ func TestProcessLogs_Move_WithoutWildcards(t *testing.T) {
 	require.Len(t, actual, 1)
 
 	require.NoError(t, plogtest.CompareLogs(expected, actual[0]))
+}
+
+func TestProcessTraces_Merge_WithoutWildcards(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	oCfg := cfg.(*Config)
+
+	oCfg.Mode = config.Merge
+	oCfg.TraceStatements = []config.ContextStatements{
+		{
+			Context: "span",
+			Attributes: []string{
+				"team",
+			},
+		},
+		{
+			Context: "resource",
+			Attributes: []string{
+				"k8s.deployment.name",
+				"k8s.namespace.name",
+				"k8s.pod.name",
+				"k8s.container.name",
+				"k8s.replicaset.name",
+				"service.name",
+				"service.version",
+				"deployment.environment.name",
+				"host.name",
+			},
+		},
+	}
+	sink := new(consumertest.TracesSink)
+	p, err := factory.CreateTraces(t.Context(), processortest.NewNopSettings(metadata.Type), oCfg, sink)
+	require.NoError(t, err)
+
+	input, err := golden.ReadTraces(filepath.Join("testdata", "traces", "input.yaml"))
+	require.NoError(t, err)
+	expected, err := golden.ReadTraces(filepath.Join("testdata", "traces", "merge", "expected-without-wildcards.yaml"))
+	require.NoError(t, err)
+
+	require.NoError(t, p.ConsumeTraces(t.Context(), input))
+
+	actual := sink.AllTraces()
+	require.Len(t, actual, 1)
+
+	require.NoError(t, ptracetest.CompareTraces(expected, actual[0]))
+}
+
+func TestProcessTraces_Move_WithoutWildcards(t *testing.T) {
+	factory := NewFactory()
+	cfg := factory.CreateDefaultConfig()
+	oCfg := cfg.(*Config)
+
+	oCfg.Mode = config.Move
+	oCfg.TraceStatements = []config.ContextStatements{
+		{
+			Context: "span",
+			Attributes: []string{
+				"team",
+			},
+		},
+		{
+			Context: "resource",
+			Attributes: []string{
+				"k8s.deployment.name",
+				"k8s.namespace.name",
+				"k8s.pod.name",
+				"k8s.container.name",
+				"k8s.replicaset.name",
+				"service.name",
+				"service.version",
+				"deployment.environment.name",
+				"host.name",
+			},
+		},
+	}
+	sink := new(consumertest.TracesSink)
+	p, err := factory.CreateTraces(t.Context(), processortest.NewNopSettings(metadata.Type), oCfg, sink)
+	require.NoError(t, err)
+
+	input, err := golden.ReadTraces(filepath.Join("testdata", "traces", "input.yaml"))
+	require.NoError(t, err)
+	expected, err := golden.ReadTraces(filepath.Join("testdata", "traces", "move", "expected-without-wildcards.yaml"))
+	require.NoError(t, err)
+
+	require.NoError(t, p.ConsumeTraces(t.Context(), input))
+
+	actual := sink.AllTraces()
+	require.Len(t, actual, 1)
+
+	require.NoError(t, ptracetest.CompareTraces(expected, actual[0]))
 }
